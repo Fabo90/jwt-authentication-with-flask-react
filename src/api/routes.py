0870e21f694603e2f52ba2d35faa8dc9c2e2af7f
Fabo90 <139248863+Fabo90@users.noreply.github.com>
2024-01-18 05:24:04 +0000

    routes

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,10 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import JWTManager
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
 
 api = Blueprint('api', __name__)
 
@@ -12,11 +16,43 @@ api = Blueprint('api', __name__)
 CORS(api)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/signup', methods=['POST', 'GET'])
+def create_user():
+    email = request.json.get('email')
+    user_name = request.json.get('user_name')
+    duplicate_email = User.query.filter_by(email=email).first()
+    duplicate_user = User.query.filter_by(user_name=user_name).first()
+    user = User(email = email,
+                user_name = user_name,
+                password = request.json.get('password'), 
+                is_active = True,)
+    if duplicate_email:
+        return jsonify({"msg":"Email already registered"}), 400 
+    elif duplicate_user:
+        return jsonify({"msg":"User already registered"}), 400
+    else:
+        db.session.add(user) 
+        db.session.commit()
+        return jsonify({"msg":"User created succesfully"}), 200
+    
+@api.route('/login', methods=['GET'])
+def login_user():
+    user_name = request.json.get('user_name')
+    password = request.json.get('password')
+    user_exist = User.query.filter_by(user_name=user_name).first()
+    password_exist = User.query.filter_by(password=password).first()
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+    if user_exist and password_exist:
+        access_token = create_access_token(identity=password)
+        return jsonify({"token": access_token}), 200
+    else:
+        return jsonify({"msg": "Incorrect user or password"}), 401
+    
+@api.route('/private')
+@jwt_required()
+def private_access():
+    password = get_jwt_identity()
+    return jsonify({"your password is": password}), 200 
 
-    return jsonify(response_body), 200
+
+
